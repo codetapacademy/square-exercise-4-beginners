ba6c067e154af92e9f415c33d5682e59d5196bcd
Marian Zburlea <marianzburlea@gmail.com>
2019-07-10 22:11:22 +0100

    finito

---
+++ src/_asset/js/test.js
@@ -0,0 +1,12 @@
+// select body
+const body = document.body
+
+// create a big square
+const bigSquare = document.createElement('div')
+bigSquare.style = {
+  border: '1px solid red'
+}
+
+body.appendChild(bigSquare)
+
+console.log(bigSquare)
